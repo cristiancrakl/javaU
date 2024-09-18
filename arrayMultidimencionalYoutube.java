f18f4aba460fa4dbbb8697d7cb8f577b19c85731
cristiancrakl <moralescadenac@gmail.com>
2024-09-17 23:35:46 -0500

    declare el scanner e hice una cosa para poder cargar datos en una matriz

--- arrayMultidimencionalYoutube.java
+++ arrayMultidimencionalYoutube.java
@@ -1,34 +1,33 @@
+import java.util.Scanner;
+
 public class arrayMultidimencionalYoutube {
 
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
-        //declaracion matriz
-        int matriz [][]=new int[3][3];
 
-        //asignacion manual
-        matriz[0][0]=5;
-        matriz[0][1]=2;
-        matriz[0][2]=6;
-        matriz[1][1]=34;
-        matriz[1][2]=65;
-        matriz[2][0]=23;
-        matriz[2][1]=22;
-        matriz[2][2]=56;
-        
-        //recorrido
+        // inicializar el scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // declaracion matriz
+        int matriz[][] = new int[3][3];
+
+        // cargar datos en la matriz
         for (int f = 0; f < matriz.length; f++) {
             for (int c = 0; c < matriz.length; c++) {
-              System.out.println("el valor de la posicion fila: "+f+" columna: "+c);
-              System.out.println("es de:"+matriz[f][c]);  
+                System.out.println("ingrese el valor de la posicion fila: " + f + " columna: " + c);
+                matriz[f][c] = scanner.nextInt();
             }
         }
 
-
-
-
-
-
-
-
+        // recorrido
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = 0; c < matriz.length; c++) {
+                System.out.println("el valor de la posicion fila: " + f + " columna: " + c);
+                System.out.println("es de:" + matriz[f][c]);
+                System.out.println("");
+            }
+            System.out.println("");
+        }
 
     }
 }
